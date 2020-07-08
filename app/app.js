186ea3e4828933e839769cdd1c651c24939bfb8a
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-08 15:06:53 +0200

    test3

--- app/app.js
+++ app/app.js
@@ -119,7 +119,7 @@ define([
 
             init: function (settings) {
 
-            	console.log("ok tam ère");
+
 
                 // destroy welcome page when app is started
                 domCtr.destroy("welcome");
@@ -130,6 +130,20 @@ define([
 
                 // get settings from choice on welcome page
                 this.settings = this.getSettingsFromUser(settings);
+                
+                console.log("bah bien sur");
+
+                var info = new OAuthInfo({
+			        // Swap this ID out with a registered application ID
+			        appId: "nCTVYx63y8WJGjxY",
+			        // Uncomment the next line and update if using your own portal
+			        // portalUrl: "https://<host>:<port>/arcgis"
+			        // Uncomment the next line to prevent the user's signed in state from being shared with other apps on the same domain with the same authNamespace value.
+			        // authNamespace: "portal_oauth_inline",
+			        popup: true
+			    });
+			    
+			    esriId.registerOAuthInfos([info]);
 
                 // set portal url
                 esriConfig.portalUrl = this.settings.url;
