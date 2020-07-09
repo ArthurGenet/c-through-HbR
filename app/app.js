fec34faaa3ac43cfc13dc00bc05049a3b580390e
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-09 11:36:02 +0200

    test26

--- app/app.js
+++ app/app.js
@@ -200,7 +200,7 @@ define([
                     // layer2 = background layer (shows remaining buildings, not selected)
 
                     // retrieve active layer from webscene
-                    this.settings.layer1 = this.scene.layers.getItemAt(9);
+                    this.settings.layer1 = this.scene.layers.getItemAt(1);
                     console.log(this.settings.layer1.title);
                     console.log(this.settings.layer1);
                     var popup = {
