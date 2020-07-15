183aa2a9a91331dc402122c5cc3ac5392ffb4130
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-15 15:06:58 +0200

    test120

--- app/VizTool.js
+++ app/VizTool.js
@@ -218,7 +218,7 @@ define([
                 console.log(highlight);
                 if (selection !== undefined && selection !== "") {
 
-                    settings.layer1.definitionExpression = settings.buildingIDname + " = "+highlight;
+                    settings.layer1.definitionExpression = "";
 
                     settings.layer2.visible = false;
 
