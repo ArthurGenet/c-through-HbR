64079452be777f6ef80c01fd979637b8043c3a1c
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-17 16:18:57 +0200

    test165

--- app/app.js
+++ app/app.js
@@ -154,111 +154,111 @@ define([
                 console.log(this.scene);
                 console.log(this.scene.initialViewProperties);
 
+                
+          //       function createSlideUI(slide, placement) {
+		        //   /*********************************************************************
+		        //    * Create a new <div> element which contains all the slide information.
+		        //    * Store a reference to the created DOM node so we can use it to place
+		        //    * other DOM nodes and connect events.
+		        //    *********************************************************************/
+		        //   var slideElement = document.createElement("div");
+		        //   // Assign the ID of the slide to the <span> element
+		        //   slideElement.id = slide.id;
+		        //   slideElement.classList.add("slide");
 
-                function createSlideUI(slide, placement) {
-		          /*********************************************************************
-		           * Create a new <div> element which contains all the slide information.
-		           * Store a reference to the created DOM node so we can use it to place
-		           * other DOM nodes and connect events.
-		           *********************************************************************/
-		          var slideElement = document.createElement("div");
-		          // Assign the ID of the slide to the <span> element
-		          slideElement.id = slide.id;
-		          slideElement.classList.add("slide");
+		        //   /*********************************************************************
+		        //    * Place the newly created DOM node cat the beginning of the slidesDiv
+		        //    *********************************************************************/
+		        //   var slidesDiv = document.getElementById("slidesDiv");
+		        //   if (placement === "first") {
+		        //     slidesDiv.insertBefore(slideElement, slidesDiv.firstChild);
+		        //   } else {
+		        //     slidesDiv.appendChild(slideElement);
+		        //   }
 
-		          /*********************************************************************
-		           * Place the newly created DOM node cat the beginning of the slidesDiv
-		           *********************************************************************/
-		          var slidesDiv = document.getElementById("slidesDiv");
-		          if (placement === "first") {
-		            slidesDiv.insertBefore(slideElement, slidesDiv.firstChild);
-		          } else {
-		            slidesDiv.appendChild(slideElement);
-		          }
+		        //   /*********************************************************************
+		        //    * Create a <div> element to contain the slide title text
+		        //    *********************************************************************/
+		        //   var title = document.createElement("div");
+		        //   title.innerText = slide.title.text;
+		        //   // Place the title of the slide in the <div> element
+		        //   slideElement.appendChild(title);
 
-		          /*********************************************************************
-		           * Create a <div> element to contain the slide title text
-		           *********************************************************************/
-		          var title = document.createElement("div");
-		          title.innerText = slide.title.text;
-		          // Place the title of the slide in the <div> element
-		          slideElement.appendChild(title);
+		        //   /*********************************************************************
+		        //    * Create a new <img> element and place it inside the newly created slide
+		        //    * element. This will reference the thumbnail from the slide.
+		        //    *********************************************************************/
+		        //   var img = new Image();
+		        //   // Set the src URL of the image to the thumbnail URL of the slide
+		        //   img.src = slide.thumbnail.url;
+		        //   // Set the title property of the image to the title of the slide
+		        //   img.title = slide.title.text;
+		        //   // Place the image inside the new <div> element
+		        //   slideElement.appendChild(img);
 
-		          /*********************************************************************
-		           * Create a new <img> element and place it inside the newly created slide
-		           * element. This will reference the thumbnail from the slide.
-		           *********************************************************************/
-		          var img = new Image();
-		          // Set the src URL of the image to the thumbnail URL of the slide
-		          img.src = slide.thumbnail.url;
-		          // Set the title property of the image to the title of the slide
-		          img.title = slide.title.text;
-		          // Place the image inside the new <div> element
-		          slideElement.appendChild(img);
+		        //   /*********************************************************************
+		        //    * Set up a click event handler on the newly created slide. When clicked,
+		        //    * the code defined below will execute.
+		        //    *********************************************************************/
+		        //   slideElement.addEventListener("click", function () {
+		        //     /*******************************************************************
+		        //      * Remove the "active" class from all elements with the .slide class
+		        //      *******************************************************************/
+		        //     var slides = document.querySelectorAll(".slide");
+		        //     Array.from(slides).forEach(function (node) {
+		        //       node.classList.remove("active");
+		        //     });
 
-		          /*********************************************************************
-		           * Set up a click event handler on the newly created slide. When clicked,
-		           * the code defined below will execute.
-		           *********************************************************************/
-		          slideElement.addEventListener("click", function () {
-		            /*******************************************************************
-		             * Remove the "active" class from all elements with the .slide class
-		             *******************************************************************/
-		            var slides = document.querySelectorAll(".slide");
-		            Array.from(slides).forEach(function (node) {
-		              node.classList.remove("active");
-		            });
+		        //     /*******************************************************************
+		        //      * Add the "active" class on the current element being selected
+		        //      *******************************************************************/
+		        //     slideElement.classList.add("active");
 
-		            /*******************************************************************
-		             * Add the "active" class on the current element being selected
-		             *******************************************************************/
-		            slideElement.classList.add("active");
+		        //     /******************************************************************
+		        //      * Applies a slide's settings to the SceneView.
+		        //      *
+		        //      * Each slide has a viewpoint and visibleLayers property that define
+		        //      * the point of view or camera for the slide and the layers that should
+		        //      * be visible to the user when the slide is selected. This method
+		        //      * allows the user to animate to the given slide's viewpoint and turn
+		        //      * on its visible layers and basemap layers in the view.
+		        //      ******************************************************************/
+		        //     slide.applyTo(this.view);
+		        //   });
+		        // }
 
-		            /******************************************************************
-		             * Applies a slide's settings to the SceneView.
-		             *
-		             * Each slide has a viewpoint and visibleLayers property that define
-		             * the point of view or camera for the slide and the layers that should
-		             * be visible to the user when the slide is selected. This method
-		             * allows the user to animate to the given slide's viewpoint and turn
-		             * on its visible layers and basemap layers in the view.
-		             ******************************************************************/
-		            slide.applyTo(this.view);
-		          });
-		        }
+		        // this.view.when(function () {
+		        //   /*********************************************************************
+		        //    * The slides will be placed in the 'slidesDiv' <div> element.
+		        //    *********************************************************************/
+		        //   document.getElementById("slidesDiv").style.visibility = "visible";
 
-		        this.view.when(function () {
-		          /*********************************************************************
-		           * The slides will be placed in the 'slidesDiv' <div> element.
-		           *********************************************************************/
-		          document.getElementById("slidesDiv").style.visibility = "visible";
+		        //   /*********************************************************************
+		        //    * The slides are a collection inside the presentation property of
+		        //    * the WebScene.
+		        //    *********************************************************************/
+		        //   var slides = scene.presentation.slides;
 
-		          /*********************************************************************
-		           * The slides are a collection inside the presentation property of
-		           * the WebScene.
-		           *********************************************************************/
-		          var slides = scene.presentation.slides;
+		        //   /*********************************************************************
+		        //    * Loop through each slide in the collection and render the slide
+		        //    *********************************************************************/
+		        //   slides.forEach(createSlideUI);
 
-		          /*********************************************************************
-		           * Loop through each slide in the collection and render the slide
-		           *********************************************************************/
-		          slides.forEach(createSlideUI);
-
-		          /*********************************************************************
-		           * Create a new slide using Slide.createFrom after clicking on the
-		           * create slide button, using the text from the title input for the
-		           * title of the slide.
-		           *********************************************************************/
+		        //   /*********************************************************************
+		        //    * Create a new slide using Slide.createFrom after clicking on the
+		        //    * create slide button, using the text from the title input for the
+		        //    * title of the slide.
+		        //    *********************************************************************/
 		          
-		        });
+		        // });
 
                 
-                var windowHitht = document.documentElement.clientHeight;
-                     toolsMenuInnerBox.style.height = windowHitht - 50 + "px";
-                     window.addEventListener("resize", function(){
-                         windowHitht = document.documentElement.clientHeight;
-                         toolsMenuInnerBox.style.height = windowHitht - 50 + "px";
-                     });
+          //       var windowHitht = document.documentElement.clientHeight;
+          //            toolsMenuInnerBox.style.height = windowHitht - 50 + "px";
+          //            window.addEventListener("resize", function(){
+          //                windowHitht = document.documentElement.clientHeight;
+          //                toolsMenuInnerBox.style.height = windowHitht - 50 + "px";
+          //            });
 
                 // wait until view is loaded
                 this.view.when(function () {
