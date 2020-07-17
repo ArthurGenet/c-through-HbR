36ac970710ef208ec907820270f6a628a1fddba0
Arthur Genet <arthurgenet@yahoo.fr>
2020-07-17 18:05:07 +0200

    test181

--- app/app.js
+++ app/app.js
@@ -232,7 +232,7 @@ define([
 		             * on its visible layers and basemap layers in the view.
 		             ******************************************************************/
 		             console.log(slide);
-		            slides.getItemAt(0).applyTo(this.view);
+		            slide.applyTo(this.view);
 		            console.log("okkkkkkkkk");
 		          });
 		        }
